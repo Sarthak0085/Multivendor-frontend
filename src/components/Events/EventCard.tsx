d3a1db479d3ef6b28da308a3f4ec3d3346a5a51e
Sarthak0085 <sarth.mahajan@gmail.com>
2024-03-06 19:16:28 +0530

    change

--- src/components/Events/EventCard.tsx
+++ src/components/Events/EventCard.tsx
@@ -69,19 +69,19 @@ const EventCard = ({ active, data }: { active?: boolean; data: IEvent }) => {
         <img src={`${data?.images[0]?.url}`} alt="" />
       </div>
       <div className="w-full lg:w-[50%] lg:mx-[30px] flex flex-col justify-center">
-        <h2 className={`${styles.productTitle}`}>{data.name}</h2>
+        <h2 className={`${styles.productTitle}`}>{data?.name}</h2>
         <p>{data.description}</p>
         <div className="flex py-2 justify-between">
           <div className="flex">
             <h5 className="font-[500] text-[18px] text-[#d55b45] pr-3 line-through">
-              {data.originalPrice}$
+              {data?.originalPrice}$
             </h5>
             <h5 className="font-bold text-[20px] text-[#333] font-Roboto">
-              {data.discountPrice}$
+              {data?.discountPrice}$
             </h5>
           </div>
           <span className="pr-3 font-[400] text-[17px] text-[#44a55e]">
-            {data.sold_out} sold
+            {data?.sold_out} sold
           </span>
         </div>
         <CountDown data={data} />
