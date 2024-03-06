12d635611157cd1f884c8271e6569248b413ba1b
Sarthak0085 <sarth.mahajan@gmail.com>
2024-03-06 19:09:09 +0530

    change name

--- src/routes/ShopRoutes.ts
+++ src/routes/ShopRoutes.ts
@@ -1,20 +1,20 @@
 import { lazy } from "react";
 
-const ShopAllOrders = lazy(() => import("../pages/shop/ShopAllOrders"));
-const ShopAllCoupons = lazy(() => import("../pages/shop/ShopAllCoupons"));
-const ShopAllEvents = lazy(() => import("../pages/shop/ShopAllEvents"));
-const ShopAllProducts = lazy(() => import("../pages/shop/ShopAllProducts"));
-const ShopAllRefunds = lazy(() => import("../pages/shop/ShopAllRefundsPage"));
-const ShopCreateEvent = lazy(() => import("../pages/shop/ShopCreateEvent"));
-const ShopCreateProduct = lazy(() => import("../pages/shop/ShopCreateProduct"));
-const ShopDashboardPage = lazy(() => import("../pages/shop/ShopDashboardPage"));
-const ShopEditProductPage = lazy(() => import("../pages/shop/ShopEditProductPage"));
-const ShopHomePage = lazy(() => import("../pages/shop/ShopHomePage"));
-const ShopOrderDetails = lazy(() => import("../pages/shop/ShopOrderDetails"));
-const ShopPreviewPage = lazy(() => import("../pages/shop/ShopPreviewPage"));
-const ShopSettingsPage = lazy(() => import("../pages/shop/ShopSettingsPage"));
-const ShopWithDrawMoneyPage = lazy(() => import("../pages/shop/ShopWithDrawMoneyPage"));
-const ShopActivationPage = lazy(() => import("../pages/shop/ShopActivationPage"));
+const ShopAllOrders = lazy(() => import("../pages/Shop/ShopAllOrders"));
+const ShopAllCoupons = lazy(() => import("../pages/Shop/ShopAllCoupons"));
+const ShopAllEvents = lazy(() => import("../pages/Shop/ShopAllEvents"));
+const ShopAllProducts = lazy(() => import("../pages/Shop/ShopAllProducts"));
+const ShopAllRefunds = lazy(() => import("../pages/Shop/ShopAllRefundsPage"));
+const ShopCreateEvent = lazy(() => import("../pages/Shop/ShopCreateEvent"));
+const ShopCreateProduct = lazy(() => import("../pages/Shop/ShopCreateProduct"));
+const ShopDashboardPage = lazy(() => import("../pages/Shop/ShopDashboardPage"));
+const ShopEditProductPage = lazy(() => import("../pages/Shop/ShopEditProductPage"));
+const ShopHomePage = lazy(() => import("../pages/Shop/ShopHomePage"));
+const ShopOrderDetails = lazy(() => import("../pages/Shop/ShopOrderDetails"));
+const ShopPreviewPage = lazy(() => import("../pages/Shop/ShopPreviewPage"));
+const ShopSettingsPage = lazy(() => import("../pages/Shop/ShopSettingsPage"));
+const ShopWithDrawMoneyPage = lazy(() => import("../pages/Shop/ShopWithDrawMoneyPage"));
+const ShopActivationPage = lazy(() => import("../pages/Shop/ShopActivationPage"));
 
 export {
     ShopAllCoupons,
